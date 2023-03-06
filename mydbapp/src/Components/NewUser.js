d92f8d46c7447e2bcdd8d16d2febc39df3f806dc
Boris Sverdlov <Borkya@gmail.com>
2023-03-07 00:52:33 +0200

    Update NewUser.js

--- mydbapp/src/Components/NewUser.js
+++ mydbapp/src/Components/NewUser.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from './Button'
 
-function NewUser() {
+const NewUser = (props) => {
+    const [userName, setUserName] = useState("");
+    const [userEmail, setUserEmail] = useState("");
+
+    const addClick = () => {
+
+        const newUser = {
+            id: Math.floor(Math.random() * 10000) + 1000, name: userName, email: userEmail,
+            address: { street: "", city: "", zipcode: "" }, hasTodos: 0
+        };
+        props.addUser(newUser);
+
+    }
+
+
     return (
-        <div>
+        <div style={{ marginTop: "130px", paddingBottom: "50px" }}>
+            <span>Add New User</span>
+            <div style={{ width: "550px", borderStyle: "solid", paddingTop: "40px", paddingBottom: "15px", borderColor: "black" }}>
+                <span className='blue-Under' style={{ marginLeft: "70px" }}>Name :</span>
+                <input style={{ marginLeft: "60px" }} type="text" onChange={(event) => setUserName(event.target.value)} />
+                <br /><br />
+                <span className='blue-Under' style={{ marginLeft: "70px" }}>Email :</span>
+                <input style={{ marginLeft: "64px" }} type="text" onChange={(event) => setUserEmail(event.target.value)} />
+                <br />
+                <div style={{ marinTop: "40px", marginLeft: "220px" }}>
+                    <Button name="Cancel" width="70px" height="30px" onClick={() => props.setShowAddUser(false)} />
+                    <Button name="Add" width="70px" height="30px" onClick={addClick} />
+                </div>
+            </div>
 
         </div>
     )
